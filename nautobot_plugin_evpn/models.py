e3228cd10dffd2e1cd58a09c4706a33eee1746ad
Graham Johnston <gramjohnston@gmail.com>
2022-09-29 03:22:11 +0000

    expanded models

--- nautobot_plugin_evpn/models.py
+++ nautobot_plugin_evpn/models.py
@@ -1,18 +1,79 @@
+from enum import unique
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
 from nautobot.core.fields import AutoSlugField
 from nautobot.core.models.generics import PrimaryModel
+
+
+class VNI(PrimaryModel):
+    vni = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(16777216)], unique=True)
+    description = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        ordering = ["vni"]
+        verbose_name = "VXLAN VNI"
+        verbose_name_plural = "VXLAN VNIs"
+
+    def get_absolute_url(self):
+        return reverse("plugins:nautobot_plugin_evpn:vni", kwargs={"pk": self.pk})
+
+    def __str__(self) -> str:
+        return f"{self.vni}"
+
+
+class EVPNLayer2VRF(PrimaryModel):
+    EVPN_VRF_TYPES = [("vlan_aware", "VLAN Aware Bundle")]
+    EVPN_ENCAPSULATION = [("vxlan", "VXLAN")]
+
+    vrf = models.OneToOneField("ipam.vrf", on_delete=models.RESTRICT)
+    type = models.CharField(max_length=20, choices=EVPN_VRF_TYPES)
+    encapsulation = models.CharField(max_length=20, choices=EVPN_ENCAPSULATION)
+    description = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        ordering = ["vrf"]
+        verbose_name = "EVPN Layer 2 VRF"
+        verbose_name_plural = "EVPN Layer 2 VRFs"
+
+    def get_absolute_url(self):
+        return reverse("plugins:nautobot_plugin_evpn:evpn-layer2-vrf", kwargs={"pk": self.pk})
+
+    def __str__(self) -> str:
+        return f"{self.vrf} type {self.type}"
+
+
+class EVPNLayer3VRF(PrimaryModel):
+    EVPN_VRF_TYPES = [("evpn_l3", "EVPN L3VPN")]
+    EVPN_ENCAPSULATION = [("vxlan", "VXLAN")]
+
+    type = models.CharField(max_length=20, choices=EVPN_VRF_TYPES)
+    encapsulation = models.CharField(max_length=20, choices=EVPN_ENCAPSULATION)
+    vrf = models.OneToOneField("ipam.vrf", on_delete=models.RESTRICT)
+    vni = models.OneToOneField(VNI, on_delete=models.RESTRICT)
+    description = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        ordering = ["vrf"]
+        verbose_name = "EVPN Layer 3 VRF"
+        verbose_name_plural = "EVPN Layer 3 VRFs"
+
+    def get_absolute_url(self):
+        return reverse("plugins:nautobot_plugin_evpn:evpn-layer3-vrf", kwargs={"pk": self.pk})
+
+    def __str__(self) -> str:
+        return f"{self.vrf} type {self.type}"
 
 
 class EVPNService(PrimaryModel):
     name = models.CharField(max_length=25)
     slug = AutoSlugField(populate_from="name")
-    vni = models.IntegerField(unique=True)
-    description = models.CharField(max_length=200, blank=True)  # , default="")
-    tenant = models.ForeignKey(
-        to="tenancy.tenant", on_delete=models.RESTRICT, blank=True, null=True
-    )
+    layer2_vrf = models.ForeignKey(EVPNLayer2VRF, on_delete=models.RESTRICT)
+    vni = models.OneToOneField(VNI, on_delete=models.RESTRICT)
+    layer3_vrf = models.ForeignKey(EVPNLayer3VRF, on_delete=models.RESTRICT, blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True)
+    tenant = models.ForeignKey(to="tenancy.tenant", on_delete=models.RESTRICT, blank=True, null=True)
 
     class Meta:
         ordering = ["vni"]
@@ -20,24 +81,7 @@ class EVPNService(PrimaryModel):
         verbose_name_plural = "EVPN Services"
 
     def get_absolute_url(self):
-        return reverse(
-            "plugins:nautobot_plugin_evpn:evpn-service", kwargs={"pk": self.pk}
-        )
+        return reverse("plugins:nautobot_plugin_evpn:evpn-service", kwargs={"pk": self.pk})
 
     def __str__(self) -> str:
-        return f"{self.name} with VNI {self.vni}"
-
-    """
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
-
-    def clean(self, *args, **kwargs):
-        if not getattr(self, "name") or not getattr(self, "vni"):
-            ValidationError("Must have a name and VNI value.")
-
-        if not getattr(self, "description"):
-            self.description = ""
-
-        super().clean(*args, **kwargs)
-"""
+        return f"{self.name}  with VNI {self.vni}"
